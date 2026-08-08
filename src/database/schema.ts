025f7497e043214de1f4629fcb713e48b8bb7958
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-08-08 11:54:43 +0200

    first commit

---
+++ src/database/schema.ts
@@ -0,0 +1,15 @@
+import {
+    pgTable,
+    uuid,
+    varchar,
+    timestamp,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+    id: uuid('id').defaultRandom().primaryKey(),
+
+    name: varchar('name', { length; 255 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull().unique(),
+
+    passwordHash
+})
